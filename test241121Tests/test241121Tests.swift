660cff4e0b49c567635bd16f834cd3cfc529c65b
Juyeong Maing <mjy9088@naver.com>
2024-11-21 18:53:35 +0900

    Initial Commit

---
+++ test241121Tests/test241121Tests.swift
@@ -0,0 +1,17 @@
+//
+//  test241121Tests.swift
+//  test241121Tests
+//
+//  Created by Juyeong Maing on 11/21/24.
+//
+
+import Testing
+@testable import test241121
+
+struct test241121Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
